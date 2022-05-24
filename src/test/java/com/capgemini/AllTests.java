94b51a2be4777e1225377e5f261b0393559a635f
diego <diego.cortinagarcia@gmail.com>
2022-05-24 10:38:59 +0200

    added suite test case

---
+++ src/test/java/com/capgemini/AllTests.java
@@ -0,0 +1,11 @@
+package com.capgemini;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ IteradoresTest.class, ProveedorTest.class })
+public class AllTests {
+
+}
