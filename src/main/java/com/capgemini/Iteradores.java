0d1442f42df1e4b85cdbbaf9913fcdd4eaa72a2e
diego <diego.cortinagarcia@gmail.com>
2022-05-24 10:30:04 +0200

    added class and test case

---
+++ src/main/java/com/capgemini/Iteradores.java
@@ -0,0 +1,62 @@
+package com.capgemini;
+
+public class Iteradores {
+	private char[] s;
+
+	public void usoFor(int[] a, int[] b) {
+
+		for (int i = 0; i < a.length; i++) {
+			a[i] = i;
+			System.out.println(a[i]);
+		}
+		System.out.println("-------");
+		for (int i = 9; i > 0; i--) {
+			b[i] = i;
+			System.out.println(b[i]);
+		}
+		System.out.println("-------");
+		for (int i = 0; i < 10; i += 2) {
+			b[i] = i;
+			System.out.println(b[i]);
+		}
+		System.out.println("-------");
+		int x = (int) (Math.random() * 100);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = x;
+			x = (int) (Math.random() * 100);
+			System.out.println(a[i]);
+		}
+	}
+
+	public void EjemploRedimensionar(int[] myArray, int[] hold) {
+
+		System.arraycopy(myArray, 0, hold, 0, myArray.length);
+		for (int i = 0; i < hold.length; i++) {
+			System.out.print(hold[i]);
+		}
+	}
+
+	public char[] createArray(char[] s) {
+
+		System.out.println((int) 'A');
+		for (int i = 0; i < 26; i++) {
+			s[i] = (char) ('A' + i);
+			System.out.println(s[i]);
+		}
+		return s;
+	}
+
+	public void imprimoNombreASCII() {
+		char[] nombre = { 'A', 'L', 'E' };
+		int[] nombreAscii = new int[nombre.length];
+		for (int i = 0; i < nombre.length; i++) {
+			nombreAscii[i] = (int) nombre[i];
+			System.out.print((int) nombre[i] + " ");
+		}
+		System.out.println();
+		System.out.println("*************");
+		for (int i = 0; i < nombreAscii.length; i++) {
+			System.out.println(nombreAscii[i]);
+		}
+	}
+}
---
+++ src/test/java/com/capgemini/IteradoresTest.java
@@ -0,0 +1,46 @@
+package com.capgemini;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IteradoresTest {
+
+	@Test
+	void testUsoFor() {
+
+		// int[] a=new int[10];
+		// int b[]=new int[10];
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEjemploRedimensionar() {
+
+		// usar estos datos para el TEST
+		// original array
+		// int[] myArray = { 1, 2, 3, 4, 5, 6 };
+		// new larger array
+		// int[] hold = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		// copy all of the myArray array to the hold
+		// array, starting with the 0th index
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCreateArray() {
+
+		// usar estos datos para el TEST
+		// s = new char[26];
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testImprimoNombreASCII() {
+		fail("Not yet implemented");
+	}
+
+}
